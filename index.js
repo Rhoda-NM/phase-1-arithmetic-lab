2a04f9db25530b44c9d42a68010adec40b44e490
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-03-25 12:21:54 +0300

    Arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
+//let multiply = num1 *num2;
 // Write your code here
+let num1 = 31;
+let num2 = 2
+const multiply = num1*num2;
+const random = Math.floor(Math.random() *10) + 1;
+console.log(random);
+
+let num3 = 28;
+let num4 = 8;
+const mod = num3 % num4;
+console.log(mod)
+let numbers = [13, 20,19,5, 9];
+const max = Math.max(...numbers)
+console.log(max)
